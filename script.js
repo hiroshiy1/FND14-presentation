e8bcee294e22a0c733de46ac7fce433bde949276
hiroshiy1 <122343802+hiroshiy1@users.noreply.github.com>
2023-02-20 20:42:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const introObj = {
 
 //47都道府県の画像データ
 const imgObj = {
-    hokkaido: ["1.png"]
+    hokkaido: ["https://github.com/hiroshiy1/FND14-presentation/blob/180a1d5408c08551f6174af334136513280feab8/1.png"]
 };
 
 //スリープ関数
